6f184f9f31bb8484145ab610fc3c6ffe06044463
Sourav Das <souravdasslg95@gmail.com>
2019-12-02 20:44:09 +0530

    feat: Initiate server and client app 1. Client side app is created using create-react-app 2. Server side app is created using express-generator 3. Defined two separate workspace for client and server 4. Implemented concurrent test for both app.

---
+++ server/jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  clearMocks: true,
+  coverageDirectory: "coverage",
+  coveragePathIgnorePatterns: [
+    "/node_modules/"
+  ],
+  coverageReporters: [
+    "html",
+  ],
+  testEnvironment: "node",
+};
