00c9cfb0fa5f94c09a0dd16da17002b082b1fd55
Sourav <souravdasslg95@gmail.com>
2019-12-04 16:33:42 +0530

    chore: Setup boilerplate for backend 1. Create models and routes directory 2. Setup logger utility 3. Connected DB with Application

--- server/app.js
+++ server/app.js
@@ -1,32 +1,38 @@
+/* eslint-disable import/no-dynamic-require */
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const fs = require('fs');
+require('dotenv').config();
+const mongoose = require('mongoose');
+const pino = require('express-pino-logger')();
+const logger = require('./utils/logger');
 
 const app = express();
-
+const modelDirPath = path.join(__dirname, 'models');
+const routeDirPath = path.join(__dirname, 'routes');
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(logger('dev'));
+app.use(pino);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
   next(createError(404));
 });
+fs.readdirSync(modelDirPath).forEach((file) => {
+  // eslint-disable-next-line global-require
+  require(`${modelDirPath}/${file}`);
+});
 
+fs.readdirSync(routeDirPath).forEach((file) => {
+  // eslint-disable-next-line global-require
+  require(`${routeDirPath}/${file}`);
+});
 // error handler
 app.use((err, req, res) => {
   // set locals, only providing error in development
@@ -36,6 +42,17 @@ app.use((err, req, res) => {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+mongoose.connect(process.env.DB_HOST, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+mongoose.connection.on('error', (err) => {
+  logger.error(err);
+});
+mongoose.connection.on('connected', () => {
+  logger.info('Connected To DB');
 });
 
 module.exports = app;
