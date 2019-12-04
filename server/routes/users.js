5c55b51013e4aa189d42b7bbf95f8310358e3972
Sourav <souravdasslg95@gmail.com>
2019-12-04 16:33:42 +0530

    chore: Setup boilerplate for backend 1. Create models and routes directory 2. Setup logger utility 3. Connected DB with Application

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,9 +2,12 @@ const express = require('express');
 
 const router = express.Router();
 
-/* GET users listing. */
+
 router.get('/', (req, res) => {
-  res.send('respond with a resource');
+  res.send('User');
 });
 
-module.exports = router;
+module.exports = {
+  path: '/user',
+  router,
+};
