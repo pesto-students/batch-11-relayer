1927636b0f6c5c77ed07a85efbdc70bbf8b19c9f
yashpandit <yashpandit1995@gmail.com>
2019-12-07 00:19:06 +0530

    Add authorize button

--- client/src/components/Dashboard/CreateNewRelay/SelectApp/SelectApp.js
+++ client/src/components/Dashboard/CreateNewRelay/SelectApp/SelectApp.js
@@ -1,19 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {
-  Col,
+  Col, Button,
 } from 'reactstrap';
 import ArrowImg from '../ArrowImg';
 import { Select, LabelItem } from '../../../common';
 
+let defaultApp = '';
+
+const onValueChange = (e) => {
+  defaultApp = e.target.value;
+};
+
+const navigate = () => {
+  const params = `scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no,
+        width=500,height=500,left=-100,top=-100`;
+  console.log(defaultApp);
+  window.open(`http://localhost:3001/authorize/${defaultApp}`, 'test', params);
+};
+
 const SelectApp = (props) => {
   const { options } = props;
   const appNames = options.map((option) => option.name);
+  // eslint-disable-next-line prefer-destructuring
+  defaultApp = appNames[0];
+
   return (
     <>
       <Col sm="12" md="4">
         <LabelItem content="Select App 1" />
-        <Select name="app1" _id="appSelect1" isRequired="true" options={appNames} />
+        <Select name="app1" _id="appSelect1" isRequired="true" options={appNames} handleChange={onValueChange} />
+        <Button className="mt-3" outline color="primary" onClick={navigate}>Authorize</Button>
       </Col>
       <ArrowImg />
       <Col sm="12" md="4">
