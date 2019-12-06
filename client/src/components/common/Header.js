0787f1f094649e24e05a50a607f07a8cfa2b7aad
Basant Soni <basantech89@gmail.com>
2019-12-06 18:03:39 +0530

    Migrate code to hackathon branch

--- client/src/components/common/Header.js
+++ client/src/components/common/Header.js
@@ -4,8 +4,8 @@ import UserIcon from '../../assets/icons/user-circle-solid.svg';
 
 const Header = () => (
   <Navbar color="primary" dark>
-    <NavbarBrand className="ml-5" href="/">Relayer</NavbarBrand>
-    <img className="mr-5" src={UserIcon} alt="user-icon" width="30px" />
+    <NavbarBrand style={{ fontSize: 'x-large' }} className="ml-0 ml-lg-5" href="/"> Relayer </NavbarBrand>
+    <img className="mr-5" src={UserIcon} alt="user-icon" width="30px" height="50px" />
   </Navbar>
 );
 
