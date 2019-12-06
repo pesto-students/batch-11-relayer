66b25e2257d4ad342b2d28d10893d94234e506fc
yashpandit <yashpandit1995@gmail.com>
2019-12-06 22:41:54 +0530

    get data for create relay-done

--- client/src/components/Dashboard/CreateNewRelay/SelectRelay.js
+++ client/src/components/Dashboard/CreateNewRelay/SelectRelay.js
@@ -2,17 +2,31 @@ import React from 'react';
 import { Row } from 'reactstrap';
 import SelectApp from './SelectApp';
 import SelectTrigger from './SelectTrigger';
+import prepareRequest from '../../../utils/requestEPLib';
+import request from '../../../utils/requestLib';
 
 class SelectRelay extends React.Component {
-  // componentDidMount() {
-  //   fetch('http://10.173.1.231:3001/api/get/all');
-  // }
+  constructor(props) {
+    super(props);
+    this.state = {
+      appDetails: [],
+    };
+  }
+
+  async componentDidMount() {
+    const requestObject = prepareRequest('getAllApps');
+    const response = await request(requestObject);
+    this.setState({
+      appDetails: response.body,
+    });
+  }
 
   render() {
+    const { appDetails } = this.state;
     return (
       <>
         <Row>
-          <SelectApp />
+          <SelectApp options={appDetails} />
         </Row>
         <Row className="mt-5">
           <SelectTrigger />
--- client/src/utils/requestEPLib.js
+++ client/src/utils/requestEPLib.js
@@ -1,8 +1,12 @@
-const baseURL = 'http://localhost:3001';
+const baseURL = 'http://10.173.1.231:3001';
 const uriObject = {
   signUp: {
     method: 'POST',
     url: `${baseURL}/user/signup`,
+  },
+  getAllApps: {
+    method: 'GET',
+    url: `${baseURL}/app/get/all`,
   },
 };
 const prepareRequest = (key) => {
