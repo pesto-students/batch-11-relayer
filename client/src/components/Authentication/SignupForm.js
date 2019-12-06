33ae4a295aacc83c340a03ae58cccd6837fd03d0
Basant Soni <basantech89@gmail.com>
2019-12-06 20:55:51 +0530

    Update SignUp v1

--- client/src/components/Authentication/SignupForm.js
+++ client/src/components/Authentication/SignupForm.js
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, { createRef } from 'react';
 import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
-import { Form, Col, Button } from 'reactstrap';
-import { InputRow } from '../common';
+import {
+  Form, Col, Button, Input, FormGroup, Label,
+} from 'reactstrap';
 
 const SignupForm = (props) => {
-  const { email, pass, signUp } = props;
+  const { signUp } = props;
+  const email = createRef();
+  const pass = createRef();
   return (
     <Form>
-      <InputRow type="email" label="Email" id="email" placeholder="username@example.com" ref={email} />
-      <InputRow type="pass" label="Password" id="pass" placeholder="P@ssw0rd" ref={pass} />
+      <FormGroup row>
+        <Label for="email" sm={12} md={2}> Email </Label>
+        <Col sm={12} md={5}>
+          <Input type="email" name="email" id="email" placeholder="username@example.com" innerRef={email} />
+        </Col>
+      </FormGroup>
+      <FormGroup row>
+        <Label for="pass" sm={12} md={2}> Email </Label>
+        <Col sm={12} md={5}>
+          <Input type="pass" name="pass" id="pass" placeholder="P@ssw0rd" innerRef={pass} />
+        </Col>
+      </FormGroup>
       <Col>
-        <Button className="mt-5 mb-1" color="primary" onClick={signUp}> Sign Up </Button>
+        <Button className="mt-5 mb-1" color="primary" onClick={(event) => signUp(event, email, pass)}> Sign Up </Button>
       </Col>
       <Col>
         Already a member? &nbsp;
--- client/src/components/common/InputRow.js
+++ client/src/components/common/InputRow.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { createRef } from 'react';
 import PropTypes from 'prop-types';
 import {
   Col, FormGroup, Input, Label,
