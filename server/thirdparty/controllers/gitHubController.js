c084a097dd18ea8e48ce66dca6d67d2b5cb8e5b8
Sourav <souravdasslg95@gmail.com>
2019-12-10 01:25:10 +0530

    WIP Github Callback

--- server/thirdparty/controllers/gitHubController.js
+++ server/thirdparty/controllers/gitHubController.js
@@ -1,5 +1,20 @@
+import eventEmitter from '../../lib/eventsLib';
+import '../../lib/slackEventLib';
+
 const webHookExecutor = (req, res) => {
-  res.send({ status: 'OK' });
+  const payload = req.body;
+  switch (payload.action) {
+    case 'created': {
+      if (Object.prototype.hasOwnProperty.call(payload, 'installation')) {
+        eventEmitter.emit('Github:Authorize:Init', payload);
+      }
+      break;
+    }
+    default: {
+      break;
+    }
+  }
+  res.send({ status: 'Ok' });
 };
 module.exports = {
   webHookExecutor,
