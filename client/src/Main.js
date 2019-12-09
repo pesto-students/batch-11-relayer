e2842b7d22e829300d794257cc29e8349b8aa7b8
Basant Soni <basantech89@gmail.com>
2019-12-09 19:35:54 +0530

    feat: Landing Page
    This commit addresses following changes
    1. Routing changes
    2. Style changes
    3. Landing Page
    
    Resolves: #33

--- client/src/Main.js
+++ client/src/Main.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import { Header } from './components/common';
-import LandingPage from './components/LandingPage';
+import LandingPage from './components/LandingPage/LandingPage';
 import { SignUp, SignIn } from './components/Authentication';
 import CreateNewRelay from './components/Dashboard/CreateNewRelay';
 import TaskHistory from './components/Dashboard/TaskHistory/TaskHistory';
@@ -11,10 +11,10 @@ const Main = () => (
     <Header />
     <Switch>
       <Route exact path="/home" component={LandingPage} />
-      <Route exact path="/signup" component={SignUp} />
-      <Route exact path="/signin" component={SignIn} />
-      <Route exact path="/create" component={CreateNewRelay} />
-      <Route exact path="/taskhistory" component={TaskHistory} />
+      <Route path="/signup" component={SignUp} />
+      <Route path="/signin" component={SignIn} />
+      <Route path="/create" component={CreateNewRelay} />
+      <Route path="/taskhistory" component={TaskHistory} />
       <Redirect from="/" to="/home" />
     </Switch>
   </>
