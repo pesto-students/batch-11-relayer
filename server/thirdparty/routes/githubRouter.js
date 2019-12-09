fadaa57314abd6d28acc71f6c353f3a1ae407671
Sourav <souravdasslg95@gmail.com>
2019-12-10 00:32:22 +0530

    Created Webhook Route For Slack

--- server/thirdparty/routes/githubRouter.js
+++ server/thirdparty/routes/githubRouter.js
@@ -5,7 +5,6 @@ const router = Router();
 
 router.post('/thirdparty/github/webhook', gitHubController.webHookExecutor);
 router.get('/thirdparty/github/auth/callback', gitHubController.authCallBackHandler);
-
 const exports = {
   path: '/',
   router,
