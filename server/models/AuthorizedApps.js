bfc6508b18e4a3971d7355b5978526ec25f020c1
Sourav <souravdasslg95@gmail.com>
2019-12-10 16:47:48 +0530

    feat: Github Authorization Implemented

--- server/models/AuthorizedApps.js
+++ server/models/AuthorizedApps.js
@@ -4,7 +4,8 @@ import shortid from 'shortid';
 const AttributeObject = new Schema(
   {
     attributeName: String,
-    attributeValue: String,
+    attributeValue: Schema.Types.Mixed,
+    attributeType: { type: String },
   }, { _id: false },
 );
 
@@ -14,6 +15,7 @@ const AuthorizedApps = new Schema({
   appName: { type: String, required: true },
   credentials: [AttributeObject],
   version: { type: String, default: '' },
+  configurationOptions: [AttributeObject],
 });
 
 export default mongoose.model('AuthorizedApps', AuthorizedApps);
