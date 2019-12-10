3fe159ee84d62fb3a21b31df82616a38a4f7876d
Sourav <souravdasslg95@gmail.com>
2019-12-10 16:47:48 +0530

    feat: Github Authorization Implemented

--- server/lib/slackEventLib.js
+++
@@ -1,6 +0,0 @@
-import mongoose from 'mongoose';
-import eventEmitter from './eventsLib';
-const authorizedApp = mongoose.model('AuthorizedApps')
-eventEmitter.on('Github:Authorize:Init', (payload) => {
-    
-});
