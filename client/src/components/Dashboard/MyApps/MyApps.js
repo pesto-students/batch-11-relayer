e28094066043c9da51087dad0c682ba8da4b33ce
yashpandit <yashpandit1995@gmail.com>
2019-12-10 21:05:51 +0530

    invalid: Remove the Edit button
    Remove the edit button from MyApps page as it is not relevant
    
    Resolves: #48

--- client/src/components/Dashboard/MyApps/MyApps.js
+++ client/src/components/Dashboard/MyApps/MyApps.js
@@ -4,7 +4,7 @@ import MyAppsHeader from './MyAppsHeader';
 import { CardWithImage } from '../../common';
 import TaskButton from '../TaskHistory/TaskButton';
 
-const css = { textAlign: 'center', marginTop: '15px' };
+const css = { marginTop: '15px' };
 
 const MyApps = () => {
   return (
@@ -12,7 +12,6 @@ const MyApps = () => {
       <MyAppsHeader />
       <Row className="mt-5">
         <CardWithImage small="12" medium="4" large="4" cardInfo={{ cardTitle: 'App Name', cardBody: 'username' }} />
-        <TaskButton content="Edit" styling={css} />
         <TaskButton content="Disconnect" styling={css} />
       </Row>
     </Container>
