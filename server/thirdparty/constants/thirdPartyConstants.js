9a10aa174a6c4291bae0cfb2175818b59136451a
RakeshUP <parthiban.rakesh95@gmail.com>
2019-12-17 18:10:54 +0530

    Changed redirect URL for OAuth

--- server/thirdparty/constants/thirdPartyConstants.js
+++ server/thirdparty/constants/thirdPartyConstants.js
@@ -1,13 +1,13 @@
 exports.SLACK_URL = 'https://slack.com/oauth/authorize?&scope=identity.basic';
 exports.SLACK_CLIENT_ID = 'client_id=47380506901.859398657013';
 exports.SLACK_SCOPE = 'scope=bot chat:write:bot';
-exports.SLACK_REDIRECT_URL = 'redirect_uri= https://4e63dfa3.ngrok.io/auth/slack/callback';
+exports.SLACK_REDIRECT_URL = 'redirect_uri= https://relayer-api.herokuapp.com/auth/slack/callback';
 exports.SLACK_AUTH_GRANT_URL = 'https://slack.com/api/oauth.access?';
 exports.SLACK_CLIENT_SECRET = 'client_secret=e1cc055910599f749f3d5a9d6fc74a94'; // Move this to some other place
 
 exports.GITHUB_URL = 'https://github.com/login/oauth/authorize?';
 exports.GITHUB_CLIENT_ID = 'client_id=7e54f4b44fd9428adb07';
 exports.GITHUB_SCOPE = 'scope=repo user notifications';
-exports.GITHUB_REDIRECT_URL = 'redirect_uri=https://4e63dfa3.ngrok.io/auth/github/callback';
+exports.GITHUB_REDIRECT_URL = 'redirect_uri=https://relayer-api.herokuapp.com/auth/github/callback';
 exports.GITHUB_AUTH_GRANT_URL = 'https://github.com/login/oauth/access_token?';
 exports.GITHUB_CLIENT_SECRET = 'client_secret=939d32fd521afb2b3628cee92f4e2a8c7c4c9665'; // Move this to some other place
