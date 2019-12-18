aac5d203c880c632f29426682e10192af016b912
rakeshup <parthiban.rakesh95@gmail.com>
2019-12-18 11:31:26 +0530

    Update thirdPartyConstants.js

--- server/thirdparty/constants/thirdPartyConstants.js
+++ server/thirdparty/constants/thirdPartyConstants.js
@@ -1,6 +1,6 @@
 exports.SLACK_URL = 'https://slack.com/oauth/authorize?&scope=identity.basic';
 exports.SLACK_CLIENT_ID = 'client_id=47380506901.859398657013';
-exports.SLACK_SCOPE = 'scope=bot chat:write:bot';
+exports.SLACK_SCOPE = 'scope=bot chat:write:bot channels:read channels:history groups:history im:history';
 exports.SLACK_REDIRECT_URL = 'redirect_uri= https://4e63dfa3.ngrok.io/auth/slack/callback';
 exports.SLACK_AUTH_GRANT_URL = 'https://slack.com/api/oauth.access?';
 exports.SLACK_CLIENT_SECRET = 'client_secret=e1cc055910599f749f3d5a9d6fc74a94'; // Move this to some other place
