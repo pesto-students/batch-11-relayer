54a7506add92ae7c381312e2351d02f70a09d948
Sourav <souravdasslg95@gmail.com>
2019-12-18 16:31:51 +0530

    Generate Relay History

--- server/lib/relayHistoryLib.js
+++ server/lib/relayHistoryLib.js
@@ -1,6 +1,11 @@
 import eventEmitter from './eventsLib';
 import RelayHistory from '../models/RelayHistory';
 
-eventEmitter.on('create:relayHistory', (payload) => {
-  RelayHistory.create(payload);
+eventEmitter.on('relayHistory:create', (payload) => {
+  const relayHistory = {
+    relayId:payload.relayId,
+    status:payload.status,
+    message: `From:${payload.from} --> To:${payload.to} Action Performed: ${payload.action}`
+  }
+  RelayHistory.create(relayHistory)
 });
